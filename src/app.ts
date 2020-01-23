364b7e0e4acc19d06823d0aa228666197a568ce7
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-23 10:52:16 +0100

    :fire: test code quality options

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ const button = document.querySelector("button");
 // addEvent for button
 if (button) {
   button.addEventListener("click", () => {
+    let userName = "Antoine";
     console.log("Clicked !!");
   });
 }
