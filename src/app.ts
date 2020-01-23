3439f631edbb71eba7833ac721979573887ef4d2
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-23 10:57:09 +0100

    :bug: fix bug

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ const button = document.querySelector("button");
 // addEvent for button
 if (button) {
   button.addEventListener("click", () => {
-    let userName = "Antoine";
     console.log("Clicked !!");
   });
 }
